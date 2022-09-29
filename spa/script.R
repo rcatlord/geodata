20b8e46565af57a701668144e62686e558a678f0
Henry Partridge <hcpartridge@uclmail.net>
2022-09-29 13:30:25 +0100

    Special Protection Areas

---
+++ spa/script.R
@@ -0,0 +1,13 @@
+# Special Protection Areas
+
+# Source: Natural England
+# URL: https://naturalengland-defra.opendata.arcgis.com/maps/special-protection-areas-england
+# Licence: Open Government Licence
+
+library(tidyverse) ; library(sf) ; library(rmapshaper)
+
+st_read("https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/Special_Protection_Areas_England/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson") %>% 
+  select(area_code = SPA_CODE, area_name = SPA_NAME) %>% 
+  st_transform(4326) %>% 
+  ms_simplify(keep = 0.001, keep_shapes = TRUE) %>% 
+  st_write("special_protection_areas.geojson")
